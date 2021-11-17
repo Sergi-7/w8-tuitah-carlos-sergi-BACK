c4558e4eed9c81904be7a7a4e488f7ffa3ac31c1
Sergi Petit <sergi_hak_7@hotmail.com>
2021-11-17 18:59:51 +0100

    Added createTweets function, and createTweets function tests

--- server/controllers/tweetsControllers.js
+++ server/controllers/tweetsControllers.js
@@ -5,6 +5,21 @@ const getTweets = async (req, res) => {
   res.json(tweets);
 };
 
+const createTweets = async (req, res, next) => {
+  try {
+    const tweet = req.body;
+    const newTweet = await Tweet.create(tweet);
+    res.json(newTweet);
+  } catch {
+    const error = new Error(
+      "There was an error when trying to create a new Tweet"
+    );
+    error.code = 400;
+    next(error);
+  }
+};
+
 module.exports = {
   getTweets,
+  createTweets,
 };
--- server/controllers/tweetsControllers.test.js
+++ server/controllers/tweetsControllers.test.js
@@ -1,8 +1,8 @@
-const { getTweets } = require("./tweetsControllers");
+const { getTweets, createTweets } = require("./tweetsControllers");
 const Tweet = require("../../database/models/tweet");
 
 describe("Given a getTweets controller", () => {
-  describe("When it an object res", () => {
+  describe("When it receives an object res", () => {
     test("Then it should call the method json", async () => {
       const tuits = [
         { text: "aaa", likes: 2, date: "14/14/14" },
@@ -18,6 +18,50 @@ describe("Given a getTweets controller", () => {
 
       expect(Tweet.find).toHaveBeenCalled();
       expect(res.json).toHaveBeenCalledWith(tuits);
+    });
+  });
+});
+
+describe("Given a createTweets controller", () => {
+  describe("When it receives a request", () => {
+    test("Then it should return a response with the new tweet created", async () => {
+      const res = {
+        json: jest.fn(),
+      };
+
+      const tuit = {
+        text: "aaa",
+        likes: 2,
+        date: "14/14/14",
+      };
+
+      const req = {
+        body: tuit,
+      };
+      Tweet.create = jest.fn().mockResolvedValue(tuit);
+
+      await createTweets(req, res);
+
+      expect(Tweet.create).toHaveBeenCalled();
+      expect(res.json).toHaveBeenCalledWith(tuit);
+    });
+  });
+  describe("When it is invoked and it rejects", () => {
+    test("Then it should call next with an error", async () => {
+      const error = { code: 400 };
+      Tweet.create = jest.fn().mockResolvedValue(null);
+      const req = {
+        body: {},
+      };
+
+      const res = {};
+      const next = jest.fn();
+
+      await createTweets(req, res, next);
+
+      expect(next).toHaveBeenCalled();
+      expect(error).toHaveProperty("code");
+      expect(error.code).toBe(400);
     });
   });
 });
