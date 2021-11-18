32385f30f8578cd287dacb2cf5fe1075e37f1c77
Cpajares <c.pajares@hotmail>
2021-11-18 19:38:06 +0100

    Included controller on end point

--- server/routes/tweetsRoutes.js
+++ server/routes/tweetsRoutes.js
@@ -4,11 +4,13 @@ const {
   createTweets,
   deleteTweet,
   addLike,
+  getTweet,
 } = require("../controllers/tweetsControllers");
 
 const router = express.Router();
 
 router.get("/", getTweets);
+router.get("/:id", getTweet);
 router.post("/new", createTweets);
 router.delete("/delete/:id", deleteTweet);
 router.patch("/like", addLike);
